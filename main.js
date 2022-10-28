ed18faf5c51136b4c73d4d4071e26cf40d5686b4
Taumah <lenitramc@gmail.com>
2022-10-28 22:41:11 +0200

    Js insert images

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let data = {
     "24.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
     "25.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
     "30.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
-    "32.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
+    // "32.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
 }
 
 let tmp = 1;
@@ -17,14 +17,14 @@ Object.entries(data).forEach(([key, value]) => {
     tmp++;
     
     let img = document.createElement("img");
-    img.src = "img/" + key;
+    img.src = "imgs/" + key;
     img.alt = value.title;
     
     if (tmp%2 == 0) {
-        document.getElementById("div#haut").appendChild(img);
+        document.getElementById("haut").appendChild(img);
     }
     else {
-        document.getElementById("div#bas").appendChild(img);
+        document.getElementById("bas").appendChild(img);
     }
 
 });
