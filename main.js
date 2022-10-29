09c2bb6c854894f7d6fb7e5fa038c6b613ce286a
Taumah <lenitramc@gmail.com>
2022-10-29 10:11:16 +0200

    Scroll auto fini

--- main.js
+++ main.js
@@ -9,10 +9,52 @@ let data = {
     "25.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
     "30.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
     "32.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
+    "5.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
+    "6.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
+    "26.png": {"title": "Titre de l'image", "desc": "Description de l'image"},
 }
 
 let tmp = 1;
 let img1 = "";
+Object.entries(data).forEach(([key, value]) => {
+    // console.log(key, value);
+    tmp++;
+    if (img1 == "" && tmp % 2 == 1) {
+        img1 = key;
+    }
+    let img = document.createElement("img");
+    img.src = "imgs/" + key;
+    img.alt = value.title;
+    // set image position to absolute
+    // img.style.position = "relative";
+    // set image position to random
+    
+    
+    
+    if (tmp%2 == 0) {
+        document.getElementById("haut").appendChild(img);
+        // img.style.left = "calc(calc(50vh - 50px)*"+ tmp-4 + ")";
+
+    }
+    else {
+        document.getElementById("bas").appendChild(img);
+        // img.style.left = "calc(calc(50vh - 50px)*"+ (tmp-3)/2 + ")";
+    }
+    
+});
+
+
+
+if (tmp%2==0) {
+    let img = document.createElement("img");
+    img.src = "imgs/" + img1;
+    img.alt = data[img1].title;
+    document.getElementById("bas").appendChild(img);
+}
+
+
+ tmp = 1;
+ img1 = "";
 Object.entries(data).forEach(([key, value]) => {
     // console.log(key, value);
     tmp++;
